facd59c7fbcdf21e6e014793f21364d3e7bb632f
kondakumar <58508420+kondakumar@users.noreply.github.com>
2023-09-26 11:05:04 +0530

    commit

--- deploymentscript.sh
+++ deploymentscript.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 ### Jenkins  pipeline need to run before execute this file
 NAMESPACE='codetest'
+export KUBECONFIG=kubeconfig;
 mkdir /home/local
 cd /home/local
 git clone https://github.com/kondakumar/CodeExample-ops.git
 cd /home/local/CodeExample-ops/overlays
 
-kubectl create namespace $NAMESPACE
+sleep 60
+NS=$(kubectl get namespace $NAMESPACE_NAME --ignore-not-found);
+if [[ "$NS" ]]; then
+  echo "Skipping creation of namespace $NAMESPACE_NAME - already exists";
+else
+  echo "Creating namespace $NAMESPACE_NAME";
+  kubectl create namespace $NAMESPACE_NAME;
+fi;
+
+sleep 30
 kubectl apply -k  ./ -n $NAMESPACE
-kubectl get all -n $NAMESPACE
+sleep 30
 curl http://localhost:8080/codetest
